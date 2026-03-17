983cc5e5224651ebd98acbc47e2cbe310beb5b40
mmittalost <mmittal@opensourcetechnologies.com>
2026-03-17 22:53:08 +0530

    change sandbox to live creds

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,15 @@
 
 export const environment = {
   production: false,
-  PAYMENT_API_BASE_URL: "https://vault-sandbox.joinblvd.com",
+  // PAYMENT_API_BASE_URL: "https://vault-sandbox.joinblvd.com",
+  PAYMENT_API_BASE_URL: "https://pci.boulevard.app",
   MAIN_URL : 'http://localhost:3010/chat', //local
   // MAIN_URL : ' https://middleware.ostlive.com/chat', //live
   //  CHECKOUT_URL : 'http://localhost:4200/checkout'
    CHECKOUT_URL : 'https://blvd-chatbot.ostlive.com/checkout',
    PAYMENT_ENCRYPTION_KEY: 'cffb12f2250bad556f81e0fda891685c6a1576386e9baf3d14f954e745f4a9cb',
-   BACKEND_URL: 'https://lg-chatbot.ostwork.com/api-token',
+  //  BACKEND_URL: 'https://lg-chatbot.ostwork.com/chat',
+   BACKEND_URL: 'http://localhost:3000/chat',
    CHAT_AGENT_URL: 'https://chat-agent.ostwork.com'
 };
 
